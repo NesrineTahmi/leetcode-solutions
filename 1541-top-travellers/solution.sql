647c2ac75798b917555bc58ab466f34c3a422e98
Nesrine <163661643+NesrineTahmi@users.noreply.github.com>
2026-04-28 23:34:51 +0000

    [LeetCode Sync] Runtime - 870 ms (34.47%), Memory - 0.0B (100.00%)

---
+++ 1541-top-travellers/solution.sql
@@ -0,0 +1,6 @@
+SELECT u.name, IFNULL(SUM(r.distance), 0) AS travelled_distance FROM Users AS u
+LEFT JOIN Rides AS r
+ON r.user_id = u.id
+GROUP BY u.id
+ORDER BY travelled_distance DESC, name ASC
+
